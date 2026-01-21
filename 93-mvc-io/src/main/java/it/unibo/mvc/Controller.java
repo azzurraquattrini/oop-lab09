54a38516b4d73e29c9a7b9f15a3c7d601a3e2904
azzurraquattrini <azzurra.quattrini@studio.unibo.it>
2026-01-21 11:16:48 +0100

    Implemented Controller in exercise 93

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,18 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
+
+    void setNextStringToPrint(String string);
+
+    String getNextStringToPrint();
+
+    void printCurrentString();
+
+    List<String> getPrintedStringsHistory();
 
 }
